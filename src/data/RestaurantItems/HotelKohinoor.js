141c3ae9b11cb46db445bf9982a3bc1a10e68788
Vaibhavi Roy <vaibhaviroy2002@gmail.com>
2023-03-19 21:56:07 +0530

    Updated Restaurant Items

---
+++ src/data/RestaurantItems/HotelKohinoor.js
@@ -0,0 +1,101 @@
+export const HotelKohinoorItems = [
+    {
+        id: 1,
+        title: "Paneer Pakora",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 170,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Enjoy the tasty and crispy Paneer Pakora !!",
+        image_uri: "https://images.unsplash.com/photo-1666190091191-0cd0c5c8c5b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 2,
+        title: "Chicken Tikka Kebab",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 190,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Chicken Tikka Kebab is a delicious appetizer that is packed with flavor",
+        image_uri: "https://images.unsplash.com/photo-1599487488170-d11ec9c172f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1250&q=80"
+    },
+    {
+        id: 3,
+        title: "Chicken Biryani",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 200,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Chicken Biryani is a savory chicken and rice dish that includes layers of chicken, rice, and aromatics that are steamed together",
+        image_uri: "https://images.unsplash.com/photo-1589302168068-964664d93dc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        id: 4,
+        title: "Mushroom Masala",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 100,
+        ratings: "4",
+        number_ratings: "47",
+        description: "This Mushroom Masala recipe is an easy, delicious Punjabi style mushroom curry made with white button mushrooms in a spiced onion-tomato gravy",
+        image_uri: "https://plus.unsplash.com/premium_photo-1671377386294-7d1336217fdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        id: 5,
+        title: "Tadka",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 50,
+        ratings: "4",
+        number_ratings: "49",
+        description: "A popular Indian dish where cooked spiced lentils are finished with a tempering made of ghee/ oil and spices",
+        image_uri: "https://images.unsplash.com/photo-1626500155537-93690c24099e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+    },
+    {
+        id: 6,
+        title: "Mixed Fried Rice",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 599,
+        ratings: "4",
+        number_ratings: "55",
+        description: "A dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat",
+        image_uri: "https://images.unsplash.com/photo-1603133872878-684f208fb84b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+    },
+    {
+        id: 7,
+        title: "Paneer Kulchaa",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 60,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Sparkling and Refreshing Beverage",
+        image_uri: "https://images.unsplash.com/photo-1611107415406-1c12f8cda424?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        id: 8,
+        title: "Chicken Butter Masala",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 135,
+        ratings: "4",
+        number_ratings: "45",
+        description: "A popular Indian dish of grilled chicken simmered in a smooth, silky and creamy onion tomato and cashew gravy",
+        image_uri: "https://images.unsplash.com/photo-1618449840665-9ed506d73a34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        id: 9,
+        title: "Veg Chowmein",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 130,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Flash-fried, then stir-fried while being tossed with additional ingredients like meat or vegetables and coated in a light sauce",
+        image_uri: "https://images.unsplash.com/photo-1617622141675-d3005b9067c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+    },
+]
---
+++ src/data/RestaurantItems/PizzaHut.js
@@ -0,0 +1,90 @@
+export const PizzaHutItems = [
+    {
+        id: 6,
+        title: "Farm House",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 459,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Delightful combination of onion, capsicum, tomato & grilled mushroom",
+        image_uri: "https://images.unsplash.com/photo-1490717064594-3bd2c4081693?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 8,
+        title: "Veggie Paradise",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 459,
+        ratings: "4",
+        number_ratings: "45",
+        description: "The awesome foursome! Golden corn, black olives, capsicum, red paprika",
+        image_uri: "https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80"
+    },
+    {
+        id: 5,
+        title: "Chicken Sausage",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 991,
+        ratings: "4",
+        number_ratings: "45",
+        description: "American classic! Spicy, herbed chicken sausage on pizza",
+        image_uri: "https://images.unsplash.com/photo-1617424771170-d333ef3d93d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=470&q=80"
+    },
+    {
+        id: 2,
+        title: "Chicken Maximus",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 699,
+        ratings: "4",
+        number_ratings: "47",
+        description: "Loaded to the Max with Chicken Tikka, Chicken Keema, Chicken Sausage and a double dose of grilled Chicken Rashers",
+        image_uri: "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 1,
+        title: "Non Veg Supreme",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 599,
+        ratings: "4",
+        number_ratings: "49",
+        description: "Supreme combination of black olives, onion, capsicum, grilled mushroom, pepper barbecue chicken, peri-peri chicken & grilled chicken rashers",
+        image_uri: "https://images.unsplash.com/photo-1593246049226-ded77bf90326?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+    },
+    {
+        id: 7,
+        title: "Indi Chicken Tikka",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 599,
+        ratings: "4",
+        number_ratings: "55",
+        description: "The wholesome flavour of tandoori masala with Chicken tikka, onion, red paprika & mint mayo",
+        image_uri: "https://images.unsplash.com/photo-1599321955726-e048426594af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        id: 4,
+        title: "Pepsi 475ml",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 60,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Sparkling and Refreshing Beverage",
+        image_uri: "https://images.unsplash.com/photo-1546695259-ad30ff3fd643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80"
+    },
+    {
+        id: 3,
+        title: "Peppy Chicken Nuggets",
+        tag: "Best Seller",
+        name: "Chicken Tikka",
+        price: 199,
+        ratings: "4",
+        number_ratings: "45",
+        description: "Oven baked soft tender boneless nuggets sprinkled with peri peri seasoning",
+        image_uri: "https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+    },
+]
